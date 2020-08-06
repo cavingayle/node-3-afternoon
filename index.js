607c993a94e9a54fdf41f580b57a7c68ddb8e1f2
Cavin Gayle <cavingayle@Cavins-MacBook-Pro.local>
2020-08-05 20:17:57 -0400

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require('express')
+const massive = require('massive')
+const ctrl = require("./controller");
+
+
+const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+    
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set('db', db)
+}).catch(err => console.log(err))
+
+app.post('/api/products', ctrl.create);
+app.get('/api/products', ctrl.getAll);
+app.get('/api/products/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.delete('/api/products/:id', ctrl.delete);
+
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`Server is listening on port ${SERVER_PORT}`))
